af4f7144f10ec3e0b40490125c3d22d5dac9d33a
William Watkins <wjwat@onionslice.org>
2022-01-10 15:37:17 -0600

    Add function to convert between fahrenheit and celsius
    Co-authored-by: Justin Dechow <justin.dechow@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-function add(number1, number2) {
-  return number1 + number2;
+function add(x, y)      { return x + y }
+function divide(x, y)   { return x / y }
+function subtract(x, y) { return x - y }
+function multiply(x, y) { return x * y }
+
+function fToC(temp) {
+	return (temp - 32) * (5/9)
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+//const number1 = parseInt(prompt("Enter a number:"));
+//const number2 = parseInt(prompt("Enter another number:"));
+//const result = add(number1, number2);
+console.log("hello!");
+console.log(add(5, 10)); // 15
+console.log(divide(10, 4)); // 2.5
+console.log(subtract(100, 25)); // 75
+console.log(multiply(2, 80)); // 160
+//alert(result);
+console.log(fToC(33));
